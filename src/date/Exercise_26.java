e42322b52682f075d378b67fc41bfa4e8fdf98be
NOEL <noel@Mac-mini-de-NOEL.local>
2021-01-25 23:45:03 +0100

    first date

--- src/date/Exercise_26.java
+++ src/date/Exercise_26.java
@@ -1,5 +1,14 @@
 package date;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Calendar;
+
 public class Exercise_26 {
 
+	public static void main(String[] args) {
+
+	
+	}
 }
